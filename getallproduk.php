fa061ee3b048c5fd635faf1720d4e6f1a51a67c3
jabs15 <51084645+jabs15@users.noreply.github.com>
2019-06-13 09:11:36 +0800

    Create getallproduk.php

---
+++ getallproduk.php
@@ -0,0 +1,161 @@
+{
+    "status": {
+        "code": 200,
+        "description": "Request OK"
+    },
+    "results": [
+        {
+            "id": "1",
+            "id_supplier": "1",
+            "nama_produk": "Kangkung ",
+            "jenis_produk": "Sayuran",
+            "harga": "3000",
+            "stok": "100",
+            "foto": "kangkung.jpg"
+        },
+        {
+            "id": "2",
+            "id_supplier": "1",
+            "nama_produk": "Bayam Hijau",
+            "jenis_produk": "Sayuran",
+            "harga": "4000",
+            "stok": "100",
+            "foto": "bayam.jpg"
+        },
+        {
+            "id": "3",
+            "id_supplier": "1",
+            "nama_produk": "Bayam Merah",
+            "jenis_produk": "Sayuran",
+            "harga": "5000",
+            "stok": "100",
+            "foto": "bayam2.jpg"
+        },
+        {
+            "id": "4",
+            "id_supplier": "1",
+            "nama_produk": "Sawi Hijau",
+            "jenis_produk": "Sayuran",
+            "harga": "5000",
+            "stok": "200",
+            "foto": "sawi1.jpg"
+        },
+        {
+            "id": "5",
+            "id_supplier": "1",
+            "nama_produk": "Sawi Putih",
+            "jenis_produk": "Sayuran",
+            "harga": "7000",
+            "stok": "200",
+            "foto": "sawi2.jpg"
+        },
+        {
+            "id": "6",
+            "id_supplier": "2",
+            "nama_produk": "Dada Ayam",
+            "jenis_produk": "Daging",
+            "harga": "20000",
+            "stok": "200",
+            "foto": "dadaayam.jpg"
+        },
+        {
+            "id": "7",
+            "id_supplier": "2",
+            "nama_produk": "Hati Ayam",
+            "jenis_produk": "Daging",
+            "harga": "5000",
+            "stok": "200",
+            "foto": "hati.jpg"
+        },
+        {
+            "id": "8",
+            "id_supplier": "2",
+            "nama_produk": "Daging Sapi (Steak)",
+            "jenis_produk": "Daging",
+            "harga": "25000",
+            "stok": "200",
+            "foto": "dagingsapi1.jpg"
+        },
+        {
+            "id": "9",
+            "id_supplier": "2",
+            "nama_produk": "Paha Ayam",
+            "jenis_produk": "Daging",
+            "harga": "25000",
+            "stok": "200",
+            "foto": "paha2.jpg"
+        },
+        {
+            "id": "10",
+            "id_supplier": "2",
+            "nama_produk": "Sayap Ayam",
+            "jenis_produk": "Daging",
+            "harga": "15000",
+            "stok": "200",
+            "foto": "sayap.jpg"
+        },
+        {
+            "id": "11",
+            "id_supplier": "3",
+            "nama_produk": "Ikan Nila",
+            "jenis_produk": "Seafood",
+            "harga": "10000",
+            "stok": "200",
+            "foto": "ikan7.jpg"
+        },
+        {
+            "id": "12",
+            "id_supplier": "3",
+            "nama_produk": "Ikan Sarden",
+            "jenis_produk": "Seafood",
+            "harga": "15000",
+            "stok": "200",
+            "foto": "ikan2.jpg"
+        },
+        {
+            "id": "13",
+            "id_supplier": "3",
+            "nama_produk": "Ikan Tongkol",
+            "jenis_produk": "Seafood",
+            "harga": "20000",
+            "stok": "200",
+            "foto": "ikan3.jpg"
+        },
+        {
+            "id": "14",
+            "id_supplier": "3",
+            "nama_produk": "Kepiting",
+            "jenis_produk": "Seafood",
+            "harga": "18000",
+            "stok": "200",
+            "foto": "kepiting.jpg"
+        },
+        {
+            "id": "15",
+            "id_supplier": "3",
+            "nama_produk": "Cumi-cumi",
+            "jenis_produk": "Seafood",
+            "harga": "25000",
+            "stok": "200",
+            "foto": "6.jpg"
+        },
+        {
+            "id": "16",
+            "id_supplier": "3",
+            "nama_produk": "Cumi-cumi A",
+            "jenis_produk": "Seafood",
+            "harga": "15000",
+            "stok": "100",
+            "foto": ""
+        },
+        {
+            "id": "17",
+            "id_supplier": "3",
+            "nama_produk": "Cumi-cumi B",
+            "jenis_produk": "Seafood",
+            "harga": "10000",
+            "stok": "100",
+            "foto": ""
+        }
+    ]
+}
